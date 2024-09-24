86658c9a12d3c1d9f473542ad6a9eb1fde7ad7e1
Arkov <144703068+Arsofall@users.noreply.github.com>
2024-09-24 09:28:56 -0400

    home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -172,16 +172,14 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
+  "The best Unblocker",
+  "Liam is my pookie",
+  "Ported by Ayden",
+  "Thanks to the original creators",
+  "Yall want chips",
+  "Check out the games tab",
   "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Yall should give me some money fr",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
